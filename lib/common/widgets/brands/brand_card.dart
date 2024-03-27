c85280b7598dad6f92df346a7809a048d72d661a
Luisangel Parra <lfaria@uninorte.edu.co>
2024-03-26 23:22:53 -0500

    Correcciones de errores y mejoras en el código

---
+++ lib/common/widgets/brands/brand_card.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:t_store/common/widgets/images/t_circular_image.dart';
+import 'package:t_store/common/widgets/texts/t_brand_title_text_with_verified_icon.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/enums.dart';
+import '../../../utils/constants/image_strings.dart';
+import '../../../utils/constants/sizes.dart';
+
+class TBrandCard extends StatelessWidget {
+  const TBrandCard({
+    super.key, this.showBorder = true, this.onTap,
+  });
+
+  final bool showBorder;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: TRoundedContainer(
+        padding: const EdgeInsets.all(TSizes.sm),
+        showBorder: showBorder,
+        backgroundColor: Colors.transparent,
+        child: Row(
+          children: [
+            Flexible(
+              child: TCIrcularImage(
+                isNetworkImage: false,
+                image: TImages.clothIcon,
+                backgroundColor: Colors.transparent,
+                overlayColor:
+                    THelperFunctions.isDarkMode(context)
+                        ? TColors.white
+                        : TColors.black,
+              ),
+            ),
+            const SizedBox(
+                width: TSizes.spaceBtwItems / 2),
+          
+            /// Text
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment:
+                    CrossAxisAlignment.start,
+                children: [
+                  const TBrandTitleWithVerifiedIcon(
+                    title: 'Nike',
+                    brandTextSize: TextSizes.large,
+                  ),
+                  Text(
+                    '25 products',
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context)
+                        .textTheme
+                        .labelMedium,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/shop/screens/store/widgets/category_tab.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/common/widgets/brands/brand_show_case.dart';
+import 'package:t_store/common/widgets/layouts/grid_layouts.dart';
+import 'package:t_store/common/widgets/products/product_cards/product_card_vertical.dart';
+import 'package:t_store/common/widgets/texts/section_heading.dart';
+
+import '../../../../../utils/constants/image_strings.dart';
+import '../../../../../utils/constants/sizes.dart';
+
+class TCategoryTab extends StatelessWidget {
+  const TCategoryTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// -- Brands --
+              const TBrandShowcase(
+                images: [
+                  TImages.productImage3,
+                  TImages.productImage2,
+                  TImages.productImage1
+                ],
+              ),
+              const TBrandShowcase(
+                images: [
+                  TImages.productImage3,
+                  TImages.productImage2,
+                  TImages.productImage1
+                ],
+              ),
+
+              /// -- Products --
+              TSectionHeading(
+                title: 'You might like',
+                showActionButton: true,
+                onPressed: () {},
+              ),
+              const SizedBox(height: TSizes.spaceBtwItems),
+
+              TGridLayaout(
+                  itemCount: 4,
+                  itemBuilder: (_, index) => const TProductCardVertical())
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
