1293f7d16db5e405ff582db71cae6a57f5cfd33a
Luisangel Parra <lfaria@uninorte.edu.co>
2024-05-12 13:47:18 -0500

    Actualizar diseño de la pantalla de perfil de usuario

--- lib/features/personalization/screens/profile/profile.dart
+++ lib/features/personalization/screens/profile/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/images/t_circular_image.dart';
 import 'package:t_store/common/widgets/texts/section_heading.dart';
@@ -26,39 +27,84 @@ class ProfileScreen extends StatelessWidget {
               width: double.infinity,
               child: Column(
                 children: [
-                  const TCIrcularImage(image: TImages.user, width: 80, height: 80),
-                  TextButton(onPressed: (){}, child: const Text('Change Profile Picture')),
-              
+                  const TCIrcularImage(
+                      image: TImages.user, width: 80, height: 80),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text('Change Profile Picture')),
                 ],
               ),
             ),
+
             /// Details
             const SizedBox(height: TSizes.spaceBtwItems / 2),
             const Divider(),
             const SizedBox(height: TSizes.spaceBtwItems),
-            const TSectionHeading(title: 'Profile Information', showActionButton: false),
+            const TSectionHeading(
+                title: 'Profile Information', showActionButton: false),
             const SizedBox(height: TSizes.spaceBtwItems),
 
-            TProfileMenu(title: 'Name', value: 'Luisangel Parra', onPressed: (){},),
-            TProfileMenu(title: 'Username', value: 'luisangel_parra', onPressed: (){},),
+            TProfileMenu(
+              title: 'Name',
+              value: 'Luisangel Parra',
+              onPressed: () {},
+            ),
+            TProfileMenu(
+              title: 'Username',
+              value: 'luisangel_parra',
+              onPressed: () {},
+            ),
 
             const SizedBox(height: TSizes.spaceBtwItems),
             const Divider(),
             const SizedBox(height: TSizes.spaceBtwItems),
 
-            const TSectionHeading(title: 'Personal Information', showActionButton: false),
+            const TSectionHeading(
+                title: 'Personal Information', showActionButton: false),
             const SizedBox(height: TSizes.spaceBtwItems),
 
-            TProfileMenu(title: 'User ID', value: '45689', onPressed: (){},),
-            TProfileMenu(title: 'E-mail', value: 'luisangel_parra', onPressed: (){},),
-            TProfileMenu(title: 'Phone Number', value: '+92-317-8059528', onPressed: (){},),
-            TProfileMenu(title: 'Gender', value: 'Male', onPressed: (){},),
-            TProfileMenu(title: 'Date of Birth', value: '11 Sep, 2003', onPressed: (){},),
+            TProfileMenu(
+              title: 'User ID',
+              value: '45689',
+              icon: Iconsax.copy,
+              onPressed: () {},
+            ),
+            TProfileMenu(
+              title: 'E-mail',
+              value: 'luisangel_parra',
+              onPressed: () {},
+            ),
+            TProfileMenu(
+              title: 'Phone Number',
+              value: '+92-317-8059528',
+              onPressed: () {},
+            ),
+            TProfileMenu(
+              title: 'Gender',
+              value: 'Male',
+              onPressed: () {},
+            ),
+            TProfileMenu(
+              title: 'Date of Birth',
+              value: '11 Sep, 2003',
+              onPressed: () {},
+            ),
+            const Divider(),
+            const SizedBox(
+              height: TSizes.spaceBtwItems,
+            ),
+
+            Center(
+              child: TextButton(
+              onPressed: () {},
+              child: const Text('Close Account',
+                  style: TextStyle(
+                    color: Colors.red,
+                  )),
+            ))
           ],
         ),
       ),
     );
   }
 }
-
-
