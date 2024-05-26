1aee5559592deda9aa10d9bc69de5648e59463ff
Luisangel Parra <lfaria@uninorte.edu.co>
2024-05-26 12:05:39 -0500

    shopping cart finished

--- lib/common/widgets/products/cart/cart_menu_icon.dart
+++ lib/common/widgets/products/cart/cart_menu_icon.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:t_store/features/shop/screens/cart/cart.dart';
+import 'package:t_store/utils/helpers/helper_functions.dart';
 
 import '../../../../utils/constants/colors.dart';
 
 class TCartCounterIcon extends StatelessWidget {
   const TCartCounterIcon({
     super.key,
+    this.iconColor,
+    this.counterBgColor,
+    this.counterTextColor,
     required this.onPressed,
-    this.iconColor = TColors.white,
   });
 
-  final Color iconColor;
+  final Color? iconColor, counterBgColor, counterTextColor;
   final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
     return Stack(children: [
       IconButton(
-          onPressed: onPressed,
+          onPressed: () => Get.to(() => const CartScreen()),
           icon: Icon(Iconsax.shopping_bag, color: iconColor)),
       Positioned(
         right: 0,
@@ -25,16 +31,16 @@ class TCartCounterIcon extends StatelessWidget {
           width: 18,
           height: 18,
           decoration: BoxDecoration(
-            color: TColors.black.withOpacity(0.5),
+            color: counterBgColor ?? (dark ? TColors.white : TColors.black),
             borderRadius: BorderRadius.circular(100),
           ),
           child: Center(
             child: Text(
               '2',
-              style: Theme.of(context)
-                  .textTheme
-                  .labelLarge!
-                  .apply(color: TColors.white, fontSizeFactor: 0.8),
+              style: Theme.of(context).textTheme.labelLarge!.apply(
+                  color: counterTextColor ??
+                      (dark ? TColors.black : TColors.white),
+                  fontSizeFactor: 0.8),
             ),
           ),
         ),
